802bcd355821fc8685340450030381eb89648bd6
nomi3 <thisisnomi3@gmail.com>
2024-05-21 14:57:01 +0900

    bcmath

--- Contests/20240519/B.php
+++ Contests/20240519/B.php
@@ -12,8 +12,8 @@ while (true) {
 $cases = array_map(function ($stdin) {
     return explode(' ', $stdin);
 }, $stdins);
-$mod = 998244353;
 
+$modStr = '998244353';
 
 foreach ($cases as $case) {
     [$A1, $A2, $C] = $case;
@@ -22,20 +22,36 @@ foreach ($cases as $case) {
     if ($B == $C || $B == $C - 1) {
         $all = 9 * 9 * 10 ** ($A + $B - 2);
         $same = 0;
+        $sameMod = 0;
         $diff = 0;
         $ans = 0;
         if ($A == $B) {
-            $same = (8 * 10 ** ($A - 1) + 1) * 4 * 10 ** ($A - 1);
+            $base = bcpow('10', $A - 1); // 10の(A-1)乗
+            $part1 = bcadd(bcmul('8', $base), '1'); // 8 * 10の(A-1)乗 + 1
+            $mod1 = bcmod($part1, $modStr);
+            $part2 = bcmul('4', $base); // 4 * 10の(A-1)乗
+            $mod2 = bcmod($part2, $modStr);
+            $same = bcmul($part1, $part2);
+            $sameMod = bcmod(bcmul($mod1, $mod2), $modStr);
         } else {
-            $same = (9 * 10 ** ($B - 1) - 10 ** $A + 1) * (10 ** $A - 1) + (10 ** $A - 1) * (10 ** $A - 2) / 2;
+            $baseA = bcpow('10', $A); // 10のA乗
+            $baseB = bcpow('10', $B - 1); // 10のB-1乗
+            $part1 = bcsub(bcmul('9', $baseB), bcpow('10', $A)); // 9 * 10の(B-1)乗 - 10のA乗
+            $part1 = bcadd($part1, '1'); // + 1
+            $part2 = bcsub(bcpow('10', $A), '1'); // 10のA乗 - 1
+            $part3 = bcsub(bcpow('10', $A), '2'); // 10のA乗 - 2
+            $part4 = bcdiv(bcmul($part3, $part2), '2'); // (10のA乗 - 2) * (10のA乗 - 1) / 2
+            $same = bcadd(bcmul($part1, $part2), $part4);
+            $sameMod = bcmod($same, $modStr);
         }
         $diff = $all - $same;
         if ($B == $C) {
-            $ans = $same;
+            $ans = $sameMod;
         } else {
-            $ans = $diff;
+            $diffMod = bcmod($diff, $modStr);
+            $ans = $diffMod;
         }
-        echo $ans % $mod . PHP_EOL;
+        echo $ans . PHP_EOL;
     } else {
         echo 0 . PHP_EOL;
     }
